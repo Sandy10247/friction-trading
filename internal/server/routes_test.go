040b1300d25004093998652d5d6fb2eb873aba60
Sandy10247 <sandilyakaravadi@gmail.com>
2026-01-08 17:51:26 +0530

    updated test case

--- internal/server/routes_test.go
+++ internal/server/routes_test.go
@@ -11,6 +11,7 @@ func TestHandler(t *testing.T) {
 	s := &Server{}
 	server := httptest.NewServer(http.HandlerFunc(s.HelloWorldHandler))
 	defer server.Close()
+
 	resp, err := http.Get(server.URL)
 	if err != nil {
 		t.Fatalf("error making request to server. Err: %v", err)
@@ -20,12 +21,14 @@ func TestHandler(t *testing.T) {
 	if resp.StatusCode != http.StatusOK {
 		t.Errorf("expected status OK; got %v", resp.Status)
 	}
-	expected := "{\"message\":\"Hello World\"}"
+
 	body, err := io.ReadAll(resp.Body)
 	if err != nil {
 		t.Fatalf("error reading response body. Err: %v", err)
 	}
-	if expected != string(body) {
-		t.Errorf("expected response body to be %v; got %v", expected, string(body))
+	want := "{\"message\":\"Hello World, from Friction Trading V2 API\"}"
+	got := string(body)
+	if want != got {
+		t.Errorf("\nwant\t:- %v\nGot\t:- %v\n", want, got)
 	}
 }
